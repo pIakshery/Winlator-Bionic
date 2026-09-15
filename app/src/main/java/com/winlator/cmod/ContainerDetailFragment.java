360c6e375f0ef438e93a38a775c4813772a73a3b
pIakshery <leradan@icloud.com>
2026-09-15 23:41:01 +0300

    Update ContainerDetailFragment.java

--- app/src/main/java/com/winlator/cmod/ContainerDetailFragment.java
+++ app/src/main/java/com/winlator/cmod/ContainerDetailFragment.java
@@ -526,7 +526,7 @@ public class ContainerDetailFragment extends Fragment implements DXVKConfigDialo
         createWinComponentsTab(view, isEditMode() ? container.getWinComponents() : Container.DEFAULT_WINCOMPONENTS);
         createDrivesTab(view);
 
-        AppUtils.setupTabLayout(view, R.id.TabLayout, R.id.LLTabWineConfiguration, R.id.LLTabWinComponents, R.id.LLTabEnvVars, R.id.LLTabDrives, R.id.LLTabAdvanced, R.id.LLTabXR);
+        AppUtils.setupTabLayout(view, R.id.TabLayout, R.id.LLTabEnvVars, R.id.LLTabAdvanced, R.id.LLTabWineConfiguration, R.id.LLTabWinComponents, R.id.LLTabDrives, R.id.LLTabXR);
 
         TabLayout tabLayout = view.findViewById(R.id.TabLayout);
 
